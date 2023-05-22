58b7526e3f5d2b414287586f345746549ec734f3
Majd Haj Yehia <majd.hajyehia@cs.huji.ac.il>
2023-05-22 12:01:08 +0300

    Initial commit

---
+++ Job.h
@@ -0,0 +1,17 @@
+//
+// Created by majd.hajyehia on 5/22/23.
+//
+
+#ifndef RESOURCES_JOB_H
+#define RESOURCES_JOB_H
+#include "MapReduceFramework.h"
+
+
+class Job{
+private:
+    JobState _state;
+public:
+
+};
+
+#endif //RESOURCES_JOB_H
